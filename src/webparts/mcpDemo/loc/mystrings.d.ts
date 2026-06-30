5e96d2cbf22fc5bd4dd0dbeef38c03e0d000531f
Aryan-Desire <aryan_desire117@outlook.com>
2026-06-30 12:38:54 +0530

    first commit

---
+++ src/webparts/mcpDemo/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IMcpDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'McpDemoWebPartStrings' {
+  const strings: IMcpDemoWebPartStrings;
+  export = strings;
+}
